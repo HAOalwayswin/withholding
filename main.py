df3e390bbdfb56a0f4be679f4c5569ae730badf8
HAOalwayswin <108118443+HAOalwayswin@users.noreply.github.com>
2023-12-14 10:38:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ from matplotlib import font_manager, rc
 from dotenv import load_dotenv
 import os
 
-# 환경 변수에서 DETA_PROJECT_KEY를 로드
-# .env 파일에서 환경 변수 로드
-load_dotenv()
 
-DETA_PROJECT_KEY = os.getenv("DETA_PROJECT_KEY")
+DETA_PROJECT_KEY = st.secrets["DETA_PROJECT_KEY"]
 if DETA_PROJECT_KEY is None:
     raise Exception("DETA_PROJECT_KEY 환경 변수가 설정되지 않았습니다.")
 deta = Deta(DETA_PROJECT_KEY)
